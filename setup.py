f28564ddb698d52442b6bc8ec9a4a0d90b173096
Tri Cao <tcao5555@gmail.com>
2026-03-11 19:20:28 -0700

    initalize project and begin testing of getting ticker responses from tiingo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="MRP",
+    version="0.0.1",
+    install_requires=[
+        "requests",
+        'importlib-metadata; python_version<"3.10"',
+    ],
+)
